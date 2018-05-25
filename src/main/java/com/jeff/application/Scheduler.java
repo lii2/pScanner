ce9f53daf18da72525f3fa40d1e04d1446728e56
lii2 <lii.jeffrey3@gmail.com>
2018-05-25 07:59:13 -0600

    Handling unknown errors

--- src/main/java/com/jeff/application/Scheduler.java
+++ src/main/java/com/jeff/application/Scheduler.java
@@ -29,7 +29,7 @@ public class Scheduler {
             }
         } catch (Exception e) {
             System.out.println("Unknown error");
-            e.printStackTrace();    
+            e.printStackTrace();
         }
     }
 
